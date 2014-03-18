6f386bfdf644f43b109415610cc434a18c3e47eb
Jorge Hortelano <jhortelano@biit-solutions.com>
2014-03-18 15:45:06 +0100

    Added some comments.

--- src/main/java/com/biit/orbeon/form/ISubmittedForm.java
+++ src/main/java/com/biit/orbeon/form/ISubmittedForm.java
@@ -3,6 +3,7 @@ package com.biit.orbeon.form;
 import java.util.List;
 
 import com.biit.orbeon.form.exceptions.CategoryDoesNotExistException;
+import com.biit.orbeon.form.exceptions.QuestionDoesNotExistException;
 
 public interface ISubmittedForm {
 
@@ -10,6 +11,13 @@ public interface ISubmittedForm {
 
 	void addCategory(ICategory category);
 
+	/**
+	 * Gets a category object from its text. If more than one category has the same text, returns the first one.
+	 * 
+	 * @param categoryText
+	 * @return
+	 * @throws CategoryDoesNotExistException
+	 */
 	ICategory getCategory(String categoryText) throws CategoryDoesNotExistException;
 
 	List<IQuestion> getQuestions();
@@ -17,7 +25,16 @@ public interface ISubmittedForm {
 	String getFormName();
 
 	String getApplicationName();
-	
+
 	String getId();
+
+	/**
+	 * Gets a Question object from its tag. If more than one question has the same text, returns the first one.
+	 * 
+	 * @param questionTag
+	 * @return
+	 * @throws QuestionDoesNotExistException
+	 */
+	IQuestion getQuestion(String questionTag) throws QuestionDoesNotExistException;
 
 }
